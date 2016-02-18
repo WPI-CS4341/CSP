2aeaef6ce1cc2db25c7ecb814be2d86b20648428
Tyler Nickerson <nickersoft@gmail.com>
2016-02-18 17:23:45 -0500

    Updated file parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os.path
+import pprint
 
 
 def main():
@@ -11,8 +12,6 @@ def main():
         filename = args[0]
         # Bags
         bags = {}
-        # Generic bag info
-        bag_info = {}
         # Items
         items = {}
         # Section tracker
@@ -28,46 +27,47 @@ def main():
                         current_section += 1
                     else:
                         s = line.split(" ")
-                        if current_section == 1: # Items
+                        if current_section == 1:  # Items
                             name = s[0]
                             weight = s[1]
                             items[name] = {"weight": weight}
-                        elif current_section == 2: # Bags
+                        elif current_section == 2:  # Bags
                             name = s[0]
                             capacity = s[1]
                             bags[name] = {"capacity": capacity}
-                        elif current_section == 3: # Fitting limits
+                        elif current_section == 3:  # Fitting limits
                             lower_bound = s[0]
                             upper_bound = s[1]
-                            bag_info["lower_bound"] = lower_bound
-                            bag_info["upper_bound"] = upper_bound
-                        elif current_section == 4: # Unary inclusive
-                            name = s[0]
-                            bags = s[1:]
-                            if not items.has_key("require_bags"):
-                                items[name]["require_bags"] = []
                             for b in bags:
+                                bags[b]["lower_bound"] = lower_bound
+                                bags[b]["upper_bound"] = upper_bound
+                        elif current_section == 4:  # Unary inclusive
+                            name = s[0]
+                            require_bags = s[1:]
+                            if "require_bags" not in items:
+                                items[name]["require_bags"] = []
+                            for b in require_bags:
                                 if b:
                                     items[name]["require_bags"].append(b)
-                        elif current_section == 5: # Unary exclusive
+                        elif current_section == 5:  # Unary exclusive
                             name = s[0]
-                            bags = s[1:]
-                            if not items.has_key("reject_bags"):
+                            reject_bags = s[1:]
+                            if "reject_bags" not in items:
                                 items[name]["reject_bags"] = []
-                            for b in bags:
+                            for b in reject_bags:
                                 if b:
                                     items[name]["reject_bags"].append(b)
-                        elif current_section == 6: # Binary equals
+                        elif current_section == 6:  # Binary equals
                             item1 = s[0]
                             item2 = s[1]
                             items[item1]["equals"] = item2
                             items[item2]["equals"] = item1
-                        elif current_section == 7: # Binary not equals
+                        elif current_section == 7:  # Binary not equals
                             item1 = s[0]
                             item2 = s[1]
                             items[item1]["not_equals"] = item2
                             items[item2]["not_equals"] = item1
-                        elif current_section == 8: # Binary
+                        elif current_section == 8:  # Binary
                             item1 = s[0]
                             item2 = s[1]
                             value1 = s[2]
@@ -76,9 +76,9 @@ def main():
                             # item1 = value1 if item2 = value2
                             items[item1]["mutex"] = [value1, item2, value2]
                             items[item2]["mutex"] = [value2, item1, value1]
-                # print bag_info
-                # print bags
-                print items
+                pp = pprint.PrettyPrinter(indent=4)
+                pp.pprint(items)
+                pp.pprint(bags)
 
         else:
             # Throw error when cannot open file
