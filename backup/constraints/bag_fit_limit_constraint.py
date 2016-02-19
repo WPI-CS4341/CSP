7dca79e05921e9f7fefab7f5b4cad10c3489f279
Harry Liu <byliuyang11@gmail.com>
2016-02-19 16:44:26 -0500

    Finished classes

--- backup/constraints/bag_fit_limit_constraint.py
+++
@@ -1,7 +0,0 @@
-class BagFitLimitContraint(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def validate(self, bag)
-        return self.x <= len(bag.items) <= self.y
---
+++ csp.py
@@ -0,0 +1,5 @@
+class CSP(object):
+    def __init__(self):
+        self.items = []
+        self.bags = []
+        self.contraints = [2]
