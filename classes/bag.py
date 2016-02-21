14d342a537c01ecc64c037f1dd6862690d2b7b06
Tyler Nickerson <nickersoft@gmail.com>
2016-02-21 12:24:21 -0500

    Added equality/inequality overrides to bags and items, as well as putInBag() function

--- classes/bag.py
+++ classes/bag.py
@@ -9,3 +9,14 @@ class Bag(object):
         self.items = []
         # Constraints of bags
         self.constraints = []
+
+    def __eq__(self, other):
+        if isinstance(other, Bag):
+            return self.name == other.name
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
--- classes/item.py
+++ classes/item.py
@@ -9,3 +9,20 @@ class Item(object):
         self.bag = None
         # Constraints of item
         self.constraints = []
+
+    def __eq__(self, other):
+        if isinstance(other, Bag):
+            return self.name == other.name
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+
+    def putInBag(self, bag):
+        if self.bag:
+            self.bag.items = [s for s in self.bag.items if s.name is not self.name]
+        bag.items.append(self)
+        self.bag = bag
